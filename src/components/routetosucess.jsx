e3fdd6c70bb955ecbd06c7e6f8bc40a7a0417d19
anandcharukesan <anandcharukesan004@gmail.com>
2024-08-18 14:06:13 +0530

    Initial Commit

---
+++ src/components/routetosucess.jsx
@@ -0,0 +1,27 @@
+// src/components/RouteToSuccess.js
+import React from 'react';
+import './routetosucess.css';
+import Tube from './tube/Tube';
+
+const RouteToSuccess = () => {
+    return (
+        <div className="landing-page">
+            <div className='successheading'>
+                <h2 className='line'>|</h2>
+                <h2 className="heading">
+                    OUR ROUTE <span className="success"> TO SUCCESS</span>
+                </h2>
+            </div>
+            <div className='tubeContainer'>
+    <Tube name="Expertise" hiddenImage="./1.png" number="1" left="0px" width="400px"/>
+    <Tube name="Agility" hiddenImage="./2.png" number="2" left="150px" width="400px"/>
+    <Tube name="Integrity" hiddenImage="./3.png" number="3" left="330px" width="400px"/>
+    <Tube current="true" name="Innovative" hiddenImage="./4.png" number="4" left="520px" width="400px"/>
+    <Tube name="Client Centric" hiddenImage="./5.png" number="5" left="760px" width="400px"/>
+    <Tube name="Collaboration" hiddenImage="./6.png" number="6" left="1000px" width="300px"/>
+</div>
+        </div>
+    );
+};
+
+export default RouteToSuccess;
---
+++ src/components/tube/Tube.jsx
@@ -0,0 +1,39 @@
+// src/components/tube/Tube.js
+import React, { useState } from 'react';
+import "./Tube.css";
+
+const Tube = (props) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        // Toggle the clicked state
+        setIsClicked(!isClicked);
+    };
+
+    const customStyle = {
+        left: props.left,
+        width: props.width,
+        backgroundColor: props.current === "true" ? "#FF6B2B" : undefined // Use navButtonBg for the current tube
+    };
+
+    const nameStyle = {
+        color: props.current === "true" ? "#fafafa" : undefined
+    };
+
+    const numStyle = {
+        color: props.current === "true" ? "rgba(243, 243, 243, 0.1)" : undefined
+    };
+
+    return (
+        <div className={`tube ${isClicked ? 'clicked' : ''}`} style={customStyle} onClick={handleClick}>
+            <p className='tubeName' style={nameStyle}>{props.name}</p>
+            <p className='tubeNumber' style={numStyle}>{props.number}</p>
+            <div className='hidden-tube'>
+                {/* Render the image in the hidden tube */}
+                <img src={props.hiddenImage} alt={props.name} className='hidden-tube-image' />
+            </div>
+        </div>
+    );
+};
+
+export default Tube;
